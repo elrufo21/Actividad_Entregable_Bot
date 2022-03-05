3aae16e672899126e4d6527a3f9e47a50bce27c8
elrufo21 <neburtnt@gmail.com>
2022-03-05 10:42:12 -0500

    v1

---
+++ registro.py
@@ -0,0 +1,12 @@
+import tkinter 
+from tkinter import *
+from tkinter import messagebox
+import pymysql
+
+pantalla = Tk()
+pantalla.geometry("300x380")
+pantalla.title("Bienvenido")
+
+
+
+pantalla.mainloop()
